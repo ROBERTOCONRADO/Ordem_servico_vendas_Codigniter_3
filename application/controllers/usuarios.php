e1e1d5872fbf469c46fefbbcb031f71bab47d1f9
RobertoConrado <robertoconradorc@gmail.com>
2022-08-13 12:18:51 -0300

    table

--- application/controllers/usuarios.php
+++ application/controllers/usuarios.php
@@ -2,14 +2,21 @@
 defined('BASEPATH') OR exit('Ação não permitida');
 
 class Usuarios extends CI_Controller {
-    publi __contruct() {
+    public function __contruct() {
         parent::__contruct();
         //Definir se há sessão
     }
     public function index() {
         $data = array(
-            'usuarios' => $this->ion_auth->users()->result(); // get all users
+            'usuarios' => $this->ion_auth->users()->result(), // get all users
         );
+        // echo '<pre>';
+        // print_r($data['usuarios']);
+        // exit();
+
+        $this->load->view('layout/header', $data);
+        $this->load->view('usuarios/index');
+        $this->load->view('layout/footer');
     }
 
 }
