c0b18faacea358aefc4afcc12d9f5980407a8342
RobertoConrado <robertoconradorc@gmail.com>
2022-08-13 12:53:04 -0300

    style

--- application/controllers/usuarios.php
+++ application/controllers/usuarios.php
@@ -8,6 +8,14 @@ class Usuarios extends CI_Controller {
     }
     public function index() {
         $data = array(
+            'styles' => array(
+                'vendor/datatables/dataTables.bootstrap4.min.css',
+            ),
+            'scripts' => array(
+                'vendor/datatables/jquery.dataTables.min.js',
+                'vendor/datatables/dataTables.bootstrap4.min.js',
+                'vendor/datatables/app.js'
+            ),
             'usuarios' => $this->ion_auth->users()->result(), // get all users
         );
         // echo '<pre>';
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -18,6 +18,12 @@
   <!-- Custom styles for this template-->
   <link href="<?php echo base_url('public/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
 
+  <?php if (isset($styles)): ?>
+    <?php foreach ($styles as $style): ?>
+      <link href="<?php echo base_url('public/' .$style); ?>" rel="stylesheet">
+    <?php endforeach; ?>
+  <?php endif; ?>
+
 </head>
 
 <body id="page-top">
