215c2bb0a9a7a53f7a7eda3acbe0530557d50bc8
RobertoConrado <robertoconradorc@gmail.com>
2022-08-17 11:08:38 -0300

    sistemas_login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,7 +3,17 @@ defined('BASEPATH') OR exit('Ação não permitida');
 
 class Login extends CI_Controller {
     public function index() {
-        $this->load->view('layout/header');
-        $this->load->view('login/index');
+
+        $identity = $this->security->xss_clean($this->input->post('email'));
+        $password = $this->security->xss_clean($this->input->post('password'));
+        $remember = FALSE; // remember the user
+        
+        if($this->ion_auth->login($identity, $password, $remember)) {
+            redirect('home');
+        }else {
+            $this->session->set_flashdata('error', 'Verifique seu e-mail ou senha');
+            $this->load->view('layout/header');
+            $this->load->view('login/index');
+        }
     }
 }
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -4,19 +4,32 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+      <div class="col-xl-6 col-lg-12 col-md-9">
 
         <div class="card o-hidden border-0 shadow-lg my-5" style="margin-top: 11rem !important">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              <div class="col-lg-12">
+                <?php if($message = $this->session->flashdata('error')): ?>
+                  <div class="row">
+                    <div class="col-md-12">
+
+                      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong><i class="fas fa-exclamation-triangle"></i>&nbsp;<?php echo $message?></strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+
+                    </div>
+                  </div>
+                <?php endif; ?>
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Seja bem-vindo!</h1>
                   </div>
-                  <form class="user" name="form-index">
+                  <form class="user" name="form-index" method="POST">
                     <div class="form-group">
                       <input type="email" name="email" class="form-control form-control-user" placeholder="Entre com seu e-mail...">
                     </div>
