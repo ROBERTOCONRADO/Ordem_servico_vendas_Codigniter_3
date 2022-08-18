936831f05b1e726ca720782c389e99d73c0f475f
RobertoConrado <robertoconradorc@gmail.com>
2022-08-18 12:47:16 -0300

    Tell

--- application/views/Sistema/index.php
+++ application/views/Sistema/index.php
@@ -21,24 +21,59 @@
               <form method="POST" name="form_edit">
                 <div class="form-group row">
                   
-                  <div class="col-md-4">
-                    <label>Nome</label>
+                  <div class="col-md-3">
+                    <label>Razão Social</label>
                     <input type="text" class="form-control" name="sistema_razao_social" placeholder="Razão social" value="<?php echo $sistema->sistema_razao_social; ?>">
                     <?php echo form_error('sistema_razao_social', '<small class="form-text text-danger">','</small>'); ?>
                   </div>
 
-                  <div class="col-md-4">
-                    <label>Sobrenome</label>
+                  <div class="col-md-3">
+                    <label>Nome Fantasia</label>
                     <input type="text" class="form-control" name="sistema_nome_fantasia" placeholder="Nome fantasia" value="<?php echo $sistema->sistema_nome_fantasia; ?>">
                     <?php echo form_error('sistema_nome_fantasia', '<small class="form-text text-danger">','</small>'); ?>
                   </div>
 
-                  <div class="col-md-4">
-                    <label>E-mail&nbsp;(Login)</label>
+                  <div class="col-md-3">
+                    <label>CNPJ</label>
                     <input type="text" class="form-control" name="sistema_cnpj" placeholder="CNPJ" value="<?php echo $sistema->sistema_cnpj; ?>">
                     <?php echo form_error('sistema_cnpj', '<small class="form-text text-danger">','</small>'); ?>
                   </div>
-                </div>  
+
+                  <div class="col-md-3">
+                    <label>Inscrição Estadual</label>
+                    <input type="text" class="form-control" name="sistema_ie" placeholder="Inscrição estadual" value="<?php echo $sistema->sistema_ie; ?>">
+                    <?php echo form_error('sistema_ie', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+                  
+                </div>
+                
+                <div class="form-group row">
+                  
+                  <div class="col-md-3">
+                    <label>Telefone Fixo</label>
+                    <input type="text" class="form-control" name="sistema_telefone_fixo" placeholder="Telefone fixo" value="<?php echo $sistema->sistema_telefone_fixo; ?>">
+                    <?php echo form_error('sistema_telefone_fixo', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                  <div class="col-md-3">
+                    <label>Telefone Móvel</label>
+                    <input type="text" class="form-control" name="sistema_telefone_movel" placeholder="Telefone móvel" value="<?php echo $sistema->sistema_telefone_movel; ?>">
+                    <?php echo form_error('sistema_telefone_movel', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                  <div class="col-md-3">
+                    <label>CNPJ</label>
+                    <input type="text" class="form-control" name="sistema_cnpj" placeholder="CNPJ" value="<?php echo $sistema->sistema_cnpj; ?>">
+                    <?php echo form_error('sistema_cnpj', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                  <div class="col-md-3">
+                    <label>Inscrição Estadual</label>
+                    <input type="text" class="form-control" name="sistema_ie" placeholder="Inscrição estadual" value="<?php echo $sistema->sistema_ie; ?>">
+                    <?php echo form_error('sistema_ie', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+                  
+                </div> 
  
                 <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
               </form>
