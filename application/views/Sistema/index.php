4fd7c9d1fbdeac6c6269bf36c78d1dccb9b14b61
RobertoConrado <robertoconradorc@gmail.com>
2022-08-18 14:31:05 -0300

    recebendo-valores

--- application/views/Sistema/index.php
+++ application/views/Sistema/index.php
@@ -62,18 +62,68 @@
                   </div>
 
                   <div class="col-md-3">
-                    <label>CNPJ</label>
-                    <input type="text" class="form-control" name="sistema_cnpj" placeholder="CNPJ" value="<?php echo $sistema->sistema_cnpj; ?>">
-                    <?php echo form_error('sistema_cnpj', '<small class="form-text text-danger">','</small>'); ?>
+                    <label>URL do Site</label>
+                    <input type="text" class="form-control" name="sistema_site_url" placeholder="URL do site" value="<?php echo $sistema->sistema_site_url; ?>">
+                    <?php echo form_error('sistema_site_url', '<small class="form-text text-danger">','</small>'); ?>
                   </div>
 
                   <div class="col-md-3">
-                    <label>Inscrição Estadual</label>
-                    <input type="text" class="form-control" name="sistema_ie" placeholder="Inscrição estadual" value="<?php echo $sistema->sistema_ie; ?>">
-                    <?php echo form_error('sistema_ie', '<small class="form-text text-danger">','</small>'); ?>
+                    <label>E-mail</label>
+                    <input type="text" class="form-control" name="sistema_email" placeholder="e-mail" value="<?php echo $sistema->sistema_email; ?>">
+                    <?php echo form_error('sistema_email', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                </div>
+                
+                <div class="form-group row">
+                  
+                  <div class="col-md-2">
+                    <label>Cidade</label>
+                    <input type="text" class="form-control" name="sistema_cidade" placeholder="Cidade" value="<?php echo $sistema->sistema_cidade; ?>">
+                    <?php echo form_error('sistema_cidade', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                  <div class="col-md-1">
+                    <label>UF</label>
+                    <input type="text" class="form-control" name="sistema_estado" placeholder="UF" value="<?php echo $sistema->sistema_estado; ?>">
+                    <?php echo form_error('sistema_estado', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+
+                  <div class="col-md-2">
+                    <label>CEP</label>
+                    <input type="text" class="form-control" name="sistema_cep" placeholder="CEP" value="<?php echo $sistema->sistema_cep; ?>">
+                    <?php echo form_error('sistema_cep', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                  <div class="col-md-3">
+                    <label>Endereço</label>
+                    <input type="text" class="form-control" name="sistema_endereco" placeholder="Endereço" value="<?php echo $sistema->sistema_endereco; ?>">
+                    <?php echo form_error('sistema_endereco', '<small class="form-text text-danger">','</small>'); ?>
                   </div>
                   
-                </div> 
+                  <div class="col-md-2">
+                    <label>Número da Residência</label>
+                    <input type="text" class="form-control" name="sistema_numero" placeholder="Número da casa" value="<?php echo $sistema->sistema_numero; ?>">
+                    <?php echo form_error('sistema_numero', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                  <div class="col-md-2">
+                    <label>Telefone Móvel</label>
+                    <input type="text" class="form-control" name="sistema_telefone_movel" placeholder="Telefone móvel" value="<?php echo $sistema->sistema_telefone_movel; ?>">
+                    <?php echo form_error('sistema_telefone_movel', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+                </div>  
+
+                <div class="form-group row">
+                  
+                  <div class="col-md-12">
+                    <label>Texto da Ordem de Serviço e Venda</label>
+                    <textarea class="form-control" name="sistema_txt_ordem_servico" placeholder="Texto da ordem de serviço e venda"><?php echo $sistema->sistema_txt_ordem_servico; ?></textarea>
+                    <?php echo form_error('sistema_txt_ordem_servico', '<small class="form-text text-danger">','</small>'); ?>
+                  </div>
+
+                </div>
  
                 <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
               </form>
