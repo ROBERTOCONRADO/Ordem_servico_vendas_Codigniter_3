3f711f7badc45ebce01b3954e2fecdcf9219ac8a
RobertoConrado <robertoconradorc@gmail.com>
2022-08-20 16:16:55 -0300

    clientes-validatons

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -56,21 +56,44 @@ class Clientes extends CI_Controller {
             [cliente_ativo] => 0
             [cliente_obs] =>
         */
-        $this->form_validation_>set_rules('cliente_nome', '', 'trim|required|min_length[4]|max_length[45]');
-        $this->form_validation_>set_rules('cliente_sobrenome', '', 'trim|required|min_length[4]|max_length[150]');
-        $this->form_validation_>set_rules('cliente_data_nascimento', '', 'required');
-        $this->form_validation_>set_rules('cliente_cpf_cnpj', '', 'trim|required|exact_length[18]');
-        $this->form_validation_>set_rules('cliente_rg_ie', '', 'trim|required|max_length[20]');
-        $this->form_validation_>set_rules('cliente_email', '', 'trim|required|valid_email|max_length[50]');
-        $this->form_validation_>set_rules('cliente_telefone', '', 'trim|max_length[14]');
-        $this->form_validation_>set_rules('cliente_celular', '', 'trim|required|max_length[15]');
-        $this->form_validation_>set_rules('cliente_cep', '', 'trim|required|exact_length[9]');
-        $this->form_validation_>set_rules('cliente_endereco', '', 'trim|required|max_length[155]');
-        $this->form_validation_>set_rules('cliente_numero_endereco', '', 'trim|max_length[20]');
-        $this->form_validation_>set_rules('cliente_bairro', '', 'trim|required|max_length[45]');
-        $this->form_validation_>set_rules('cliente_complemento', '', 'trim|max_length[145]');
-        $this->form_validation_>set_rules('cliente_cidade', '', 'trim|required|max_length[50]');
-        $this->form_validation_>set_rules('cliente_estado', '', 'trim|required|exact_length[2]');
-        $this->form_validation_>set_rules('cliente_obs', '', 'max_length[500]');
+        $this->form_validation->set_rules('cliente_nome', '', 'trim|required|min_length[4]|max_length[45]');
+        $this->form_validation->set_rules('cliente_sobrenome', '', 'trim|required|min_length[4]|max_length[150]');
+        $this->form_validation->set_rules('cliente_data_nascimento', '', 'required');
+        $this->form_validation->set_rules('cliente_cpf_cnpj', '', 'trim|required|exact_length[18]');
+        $this->form_validation->set_rules('cliente_rg_ie', '', 'trim|required|max_length[20]');
+        $this->form_validation->set_rules('cliente_email', '', 'trim|required|valid_email|max_length[50]');
+        $this->form_validation->set_rules('cliente_telefone', '', 'trim|max_length[14]');
+        $this->form_validation->set_rules('cliente_celular', '', 'trim|required|max_length[15]');
+        $this->form_validation->set_rules('cliente_cep', '', 'trim|required|exact_length[9]');
+        $this->form_validation->set_rules('cliente_endereco', '', 'trim|required|max_length[155]');
+        $this->form_validation->set_rules('cliente_numero_endereco', '', 'trim|max_length[20]');
+        $this->form_validation->set_rules('cliente_bairro', '', 'trim|required|max_length[45]');
+        $this->form_validation->set_rules('cliente_complemento', '', 'trim|max_length[145]');
+        $this->form_validation->set_rules('cliente_cidade', '', 'trim|required|max_length[50]');
+        $this->form_validation->set_rules('cliente_estado', '', 'trim|required|exact_length[2]');
+        $this->form_validation->set_rules('cliente_obs', '', 'max_length[500]');
+
+        if($this->form_validation->run()){
+            echo '<pre';print_r($this->input->post());exit();
+        }else {
+            //Erro de validação
+            $data = array(
+
+                'titulo' => 'Atualizar cliente',
+
+                'scripts' => array(
+                    'vendor/mask/jquery.mask.min.js',
+                    'vendor/mask/app.js',
+                ),
+    
+                'cliente' => $this->core_model->get_by_id('clientes', array('cliente_id' => $cliente_id)),
+            );
+             echo '<pre>';print_r($data['cliente']);exit();
+            
+            $this->load->view('layout/header', $data);
+            $this->load->view('clientes/edit');
+            $this->load->view('layout/footer');
+        }
     }
+}
 }
