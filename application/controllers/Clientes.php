fa8716c53d4f267c3bbf51ff339affbac8755a96
RobertoConrado <robertoconradorc@gmail.com>
2022-08-20 15:12:35 -0300

    editar-clientes

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -1,2 +1,31 @@
 <?php 
 defined('BASEPATH') OR exit('Ação não permitida!');
+
+class Clientes extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        if(!$this->ion_auth->logged_in()) {
+            $this->session->set_flashdata('info', 'Sua sessão expirou!');
+            redirect('login');
+        }
+    }
+    public function index() {
+        $data = array(
+
+            'titulo' => 'Clientes cadastrados',
+
+            'styles' => array(
+                'vendor/datatables/dataTables.bootstrap4.min.css',
+            ),
+            'scripts' => array(
+                'vendor/datatables/jquery.dataTables.min.js',
+                'vendor/datatables/dataTables.bootstrap4.min.js',
+                'vendor/datatables/app.js'
+            ),
+            'clientes' => $this->core_model->get+all('clientes'),
+        );
+        $this->load->view('layout/header', $data);
+        $this->load->view('clientes/index');
+        $this->load->view('layout/footer');
+    }
+}
