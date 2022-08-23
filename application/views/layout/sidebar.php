737a40c5186a827554a3334aa864f8cb534a37da
RobertoConrado <robertoconradorc@gmail.com>
2022-08-23 14:14:57 -0300

    cadastros

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -26,6 +26,20 @@
       <div class="sidebar-heading">
       &nbsp;Cadastros
       </div>
+      
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-user-tie"></i>
+          <span>Cadastros</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Escolha uma opção</h6>
+            <a class="collapse-item" href="<?php echo base_url('clientes'); ?>">Buttons</a>
+            <a class="collapse-item" href="cards.html">Cards</a>
+          </div>
+        </div>
+      </li>
 
       <li class="nav-item">
         <a title="Gerenciar clientes" class="nav-link" href="<?php echo base_url('clientes'); ?>">
