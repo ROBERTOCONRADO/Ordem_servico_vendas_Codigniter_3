fccb9e9524feba7fa6c8556100c397c577745529
RobertoConrado <robertoconradorc@gmail.com>
2022-08-24 15:07:49 -0300

    fornecedores

--- application/controllers/Fornecedores.php
+++ application/controllers/Fornecedores.php
@@ -9,3 +9,25 @@ class Clientes extends CI_Controller {
             redirect('login');
         }
     }
+    public function index() {
+        $data = array(
+
+            'titulo' => 'Fornecedores cadastrados',
+
+            'styles' => array(
+                'vendor/datatables/dataTables.bootstrap4.min.css',
+            ),
+            'scripts' => array(
+                'vendor/datatables/jquery.dataTables.min.js',
+                'vendor/datatables/dataTables.bootstrap4.min.js',
+                'vendor/datatables/app.js'
+            ),
+            'fornecedores' => $this->core_model->get_all('fornecedores'),
+        );
+        // echo '<pre>';print_r($data['fornecedores']);exit();
+        
+        $this->load->view('layout/header', $data);
+        $this->load->view('fornecedores/index');
+        $this->load->view('layout/footer');
+    }
+}    
