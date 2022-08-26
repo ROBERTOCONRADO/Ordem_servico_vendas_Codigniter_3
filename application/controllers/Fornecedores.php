37b1b7a437e83a90ebe0178e7ed22c41a2a0668a
RobertoConrado <robertoconradorc@gmail.com>
2022-08-26 13:56:31 -0300

    configs

--- application/controllers/Fornecedores.php
+++ application/controllers/Fornecedores.php
@@ -37,15 +37,15 @@ class Fornecedores extends CI_Controller {
             redirect('fornecedores');
         }else {
             $this->form_validation->set_rules('fornecedor_razao', '', 'trim|required|min_length[4]|max_length[200]|callback_check_razao_social');
-            $this->form_validation->set_rules('fornecedor_nome_fantasia', '', 'trim|required|min_length[4]|max_length[145]|callback_check_nome_fantasia');
-            $this->form_validation->set_rules('fornecedor_data_nascimento', '', 'required');
 
-            
-            $this->form_validation->set_rules('fornecedor_cnpj', '', 'trim|required|exact_length[18]|callback_valida_cnpj');
-            $this->form_validation->set_rules('fornecedor_ie', '', 'trim|required|max_length[20]|callback_check_ie');
-            $this->form_validation->set_rules('fornecedor_email', '', 'trim|required|valid_email|max_length[50]|callback_check_email');
-            $this->form_validation->set_rules('fornecedor_telefone', '', 'trim|required|max_length[14]|callback_check_telefone');
-            $this->form_validation->set_rules('fornecedor_celular', '', 'trim|required|max_length[15]|callback_check_celular');
+
+
+            // $this->form_validation->set_rules('fornecedor_nome_fantasia', '', 'trim|required|min_length[4]|max_length[145]|callback_check_nome_fantasia');
+            // $this->form_validation->set_rules('fornecedor_cnpj', '', 'trim|required|exact_length[18]|callback_valida_cnpj');
+            // $this->form_validation->set_rules('fornecedor_ie', '', 'trim|required|max_length[20]|callback_check_ie');
+            // $this->form_validation->set_rules('fornecedor_email', '', 'trim|required|valid_email|max_length[50]|callback_check_email');
+            // $this->form_validation->set_rules('fornecedor_telefone', '', 'trim|required|max_length[14]|callback_check_telefone');
+            // $this->form_validation->set_rules('fornecedor_celular', '', 'trim|required|max_length[15]|callback_check_celular');
             $this->form_validation->set_rules('fornecedor_cep', '', 'trim|required|exact_length[9]');
             $this->form_validation->set_rules('fornecedor_endereco', '', 'trim|required|max_length[155]');
             $this->form_validation->set_rules('fornecedor_numero_endereco', '', 'trim|max_length[20]');
@@ -71,6 +71,134 @@ class Fornecedores extends CI_Controller {
                 $this->load->view('fornecedores/edit');
                 $this->load->view('layout/footer');
             }  
+        }
+    }
+    public function check_razao_social($fornecedor_razao) {
+        $fornecedor_id = $this->input->post('fornecedor_id');
+        if($this->core_model->get_by_id('fornecedores', array('fornecedor_razao' => $fornecedor_razao, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_razao_social', 'Esta razão social já existe!');
+            return FALSE;
+        }else {
+            return TRUE;
+        }
+    }
+    public function check_ie($fornecedor_ie) {
+        $fornecedor_id = $this->input->post('fornecedor_id');
+        if($this->core_model->get_by_id('fornecedores', array('fornecedor_ie' => $fornecedor_ie, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_ie', 'Esse documento já existe!');
+            return FALSE;
+        }else {
+            return TRUE;
+        }
+    }
+    public function check_email($fornecedor_email) {
+        $fornecedor_id = $this->input->post('fornecedor_id');
+        if($this->core_model->get_by_id('fornecedores', array('fornecedor_email' => $fornecedor_email, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_email', 'Esse e-mail já existe!');
+            return FALSE;
+        }else {
+            return TRUE;
+        }
+    }
+    public function check_telefone($fornecedor_telefone) {
+        $fornecedor_id = $this->input->post('fornecedor_id');
+        if($this->core_model->get_by_id('fornecedores', array('fornecedor_telefone' => $fornecedor_telefone, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_telefone', 'Esse telefone já existe!');
+            return FALSE;
+        }else {
+            return TRUE;
+        }
+    }
+    public function check_celular($fornecedor_celular) {
+        $fornecedor_id = $this->input->post('fornecedor_id');
+        if($this->core_model->get_by_id('fornecedores', array('fornecedor_celular' => $fornecedor_celular, 'fornecedor_id !=' => $fornecedor_id))) {
+            $this->form_validation->set_message('check_celular', 'Número celular já existente!');
+            return FALSE;
+        }else {
+            return TRUE;
+        }
+    }
+    public function valida_cnpj($cnpj) {
+
+        // Verifica se um número foi informado
+        if (empty($cnpj)) {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+        }
+
+        if ($this->input->post('cliente_id')) {
+
+            $cliente_id = $this->input->post('cliente_id');
+
+            if ($this->core_model->get_by_id('clientes', array('cliente_id !=' => $cliente_id, 'cliente_cpf_cnpj' => $cnpj))) {
+                $this->form_validation->set_message('valida_cnpj', 'Esse CNPJ já existe');
+                return FALSE;
+            }
+        }
+
+        // Elimina possivel mascara
+        $cnpj = preg_replace("/[^0-9]/", "", $cnpj);
+        $cnpj = str_pad($cnpj, 14, '0', STR_PAD_LEFT);
+
+
+        // Verifica se o numero de digitos informados é igual a 11 
+        if (strlen($cnpj) != 14) {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+        }
+
+        // Verifica se nenhuma das sequências invalidas abaixo 
+        // foi digitada. Caso afirmativo, retorna falso
+        else if ($cnpj == '00000000000000' ||
+                $cnpj == '11111111111111' ||
+                $cnpj == '22222222222222' ||
+                $cnpj == '33333333333333' ||
+                $cnpj == '44444444444444' ||
+                $cnpj == '55555555555555' ||
+                $cnpj == '66666666666666' ||
+                $cnpj == '77777777777777' ||
+                $cnpj == '88888888888888' ||
+                $cnpj == '99999999999999') {
+            $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+            return false;
+
+            // Calcula os digitos verificadores para verificar se o
+            // CPF é válido
+        } else {
+
+            $j = 5;
+            $k = 6;
+            $soma1 = "";
+            $soma2 = "";
+
+            for ($i = 0; $i < 13; $i++) {
+
+                $j = $j == 1 ? 9 : $j;
+                $k = $k == 1 ? 9 : $k;
+
+                //$soma2 += ($cnpj{$i} * $k);
+
+                //$soma2 = intval($soma2) + ($cnpj{$i} * $k); //Para PHP com versão < 7.4
+                $soma2 = intval($soma2) + ($cnpj[$i] * $k); //Para PHP com versão > 7.4
+
+                if ($i < 12) {
+                    //$soma1 = intval($soma1) + ($cnpj{$i} * $j); //Para PHP com versão < 7.4
+                    $soma1 = intval($soma1) + ($cnpj[$i] * $j); //Para PHP com versão > 7.4
+                }
+
+                $k--;
+                $j--;
+            }
+
+            $digito1 = $soma1 % 11 < 2 ? 0 : 11 - $soma1 % 11;
+            $digito2 = $soma2 % 11 < 2 ? 0 : 11 - $soma2 % 11;
+
+            if (!($cnpj[12] == $digito1) and ( $cnpj[13] == $digito2)) {
+                $this->form_validation->set_message('valida_cnpj', 'Por favor digite um CNPJ válido');
+                return false;
+            } else {
+                return true;
+            }
         }
     }
 }    
